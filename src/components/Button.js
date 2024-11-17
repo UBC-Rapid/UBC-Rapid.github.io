1726361a4ef25c1431a8d7fb82a6c7487c8ba7ef
Lo3wer <leozhang226@gmail.com>
2024-11-16 16:07:00 -0800

    larger buttons!

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 const STYLES = ['btn--primary', 'btn--outline'];
-const SIZES = ['btn--medium'];
+const SIZES = ['btn--medium','btn--large'];
 
 export const Button = ({
     children,
