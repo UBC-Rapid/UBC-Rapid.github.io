171ab791901308e6fa3eef774845483422f34e58
Jordyn Knock <jordynknock@Jordyns-MacBook-Air.local>
2024-12-02 10:42:32 -0800

    IMAGES WITHOUT BG (1& 2)

--- src/components/pages/Consulting.js
+++ src/components/pages/Consulting.js
@@ -28,8 +28,8 @@ export default function Consulting() {
                 <br /><br /> 
                 <div className='consulting_img_wrapper'>
 
-                    <img src="/images/Model1.png" alt="Model 1" />
-                    <img src="/images/Model2.png" alt="Model 2" />
+                    <img src="/images/Model4_bg.png" alt="Model 4" />
+                    <img src="/images/Model2_bg.png" alt="Model 2" />
                     <img src="/images/Model3.png" alt="Model 3" />
                 </div>
                 <h3>1. SUBMIT FORM</h3>
