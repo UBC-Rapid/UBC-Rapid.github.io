37ec011356a7f071f9756bc4d6accaba7bda287e
winterkube <theicekubex@gmail.com>
2025-01-25 14:05:54 -0800

    fixed card links

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ import JoinUs from'./components/pages/JoinUs';
 import Projects from'./components/pages/Projects';
 
 function App() {
-  return (
-    <>
-      <HashRouter>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={About}/>
-          <Route path='/printing' exact component={Printing}/>
-          <Route path='/modelling' exact component={Consulting}/>
-          <Route path='/projects' exact component={Projects}/>
-          <Route path='/join-us' exact component={JoinUs}/>
-        </Switch>
-        <Footer />
-      </HashRouter>
-    </>
-  );
+    return (
+        <>
+            <HashRouter>
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact component={About}/>
+                    <Route path='/printing' exact component={Printing}/>
+                    <Route path='/modelling' exact component={Consulting}/>
+                    <Route path='/projects' exact component={Projects}/>
+                    <Route path='/join-us' exact component={JoinUs}/>
+                </Switch>
+                <Footer />
+            </HashRouter>
+        </>
+    );
 }
 
 export default App;
--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom';
 import './Cards.css';
 
 function CardItem(props) {
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
         <>
             <li className='cards__item'>
-                <Link className='cards__item__link' to={props.path}>
+                <Link className='cards__item__link' to={props.path} onClick={scrollToTop}>
                     <figure className='cards__item__pic-wrap' data-category={props.label}>
                         <img src={props.src} alt="Rapid"
                         className='cards__item__img'/>
