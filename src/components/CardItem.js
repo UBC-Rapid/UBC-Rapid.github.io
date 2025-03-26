9e76af03f5ffe41c016780bd71d651eb8965e1b4
winterkube <theicekubex@gmail.com>
2025-03-26 02:48:13 -0700

    added filament recycler photos + made images square

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import {fade} from 'react-slideshow-image';
 import './Cards.css';
 
 function CardItem(props) {
