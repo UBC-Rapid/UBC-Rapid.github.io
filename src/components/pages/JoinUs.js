27c5487562be209f19d0d5bf1b1236af936d0506
Milun Gracias-Taplay <44041180+m1lun@users.noreply.github.com>
2025-08-27 01:51:03 -0400

    applications 2025/26

--- src/components/pages/JoinUs.js
+++ src/components/pages/JoinUs.js
@@ -9,11 +9,10 @@ export default function JoinUs() {
             <h1 className='join-us'>JOIN US</h1>
             <div className='joinus_wrapper'>
             <br /><br />
-                <h3>The applications for 2024/25 have closed. Applications will open for 2025/26 in September</h3>
+                <h3>The applications for 2025/26 are now open. Applications will close on September 12th at 11:59pm</h3>
                 <br /><br />
                 <p>We are recruiting members to work in our sub-teams, as the demands for 
                     CAD Modelling, printer repair and 3D Printing orders have increased over the years. 
-                    We are also hiring for outreach positions. 
                     New members will be attending our 
                     workshops to learn prototyping and 3d printing basics. Once they gain adequate 
                     experience, they will be welcome to contribute to our service or project sub-teams.
@@ -27,7 +26,7 @@ export default function JoinUs() {
                     Regardless of experience level, we are looking for dedicated 
                     individuals willing to put in the time and effort to learn.
                 </p>
-                {/* <p>Linked below is the application form for UBC Rapid. Please answer 
+                <p>Linked below is the application form for UBC Rapid. Please answer 
                     all the required questions. If selected for an interview, you 
                     will receive an email during the application time frame to                             
                     book an interview. The interview is expected to take up to 
@@ -39,16 +38,15 @@ export default function JoinUs() {
                     <br /><br />
                 </p>
                 <br /><br />
-                UPDATE APPLICATION FORM LINK HERE!
                 <div className="info-btns-joinus">
                     <Button
                         className='btns'
                         buttonStyle='btn--primary'
                         buttonSize='btn--large'
                         newTab='true'
-                        link='https://forms.gle/RYVh95frtJTTT3UR6'
+                        link='https://forms.gle/rLQVo1o8oWQrb2Fw6'
                     >
-                        UBC Rapid Application Form
+                        Application Form
                     </Button>
                 </div>
                 <br /><br />
@@ -57,7 +55,7 @@ export default function JoinUs() {
                     business/accounting skills. The most important thing we are 
                     looking for are dedicated people that will grow with the team.<br /><br />
                     We look forward to meeting you!
-                </p> */}
+                </p>
                 <br /><br />
             </div>
         </>
