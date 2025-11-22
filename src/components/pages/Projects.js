0f943a11cf66e31eef45ed7586fa561b8f6b8e03
Milun Gracias-Taplay <44041180+m1lun@users.noreply.github.com>
2025-11-22 00:56:30 -0500

    november merge

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,76 +3,97 @@ import '../../App.css';
 import './Projects.css';
 
 export default function Projects() {
-    return (
-        <>
-            <h1 className='projects'>PROJECTS</h1>
-            <div className='projects_wrapper'>
-                <h3>FILAMENT RECYCLER DESIGN AND CONSTRUCTION</h3>
-                <p> To make 3D printing a more sustainable practice within the team and in the broader UBC community, 
-                    our team is aiming to design and build a Filament Recycler. This innovative system transforms 
-                    3D printing waste, including failed prints and supports, into fresh filament rolls. The entire system 
-                    consists of three key machines: a shredder that reduces the plastic into granules, an extruder that 
-                    then melts the shredded plastic and shapes it into the correct filament size, and a spooler that 
-                    winds the filament back into rolls. Recent progress includes completing the preliminary design of our 
-                    shredder and extruder, as well as procuring some parts to be used towards the build phase. 
-                </p>
-                <div className='consulting_img_wrapper project'>
-                    <img src="/images/filament_recycler1.jpg" alt="1" />
-                    <img src="/images/filament_recycler2.jpg" alt="2" />
-                    <img src="/images/filament_recycler3.jpg" alt="3" />
-                </div>
-                <br></br>
-                <h3>MAINTENANCE</h3>
-                <p>Working and well-calibrated 3D printers are the core of UBC Rapid’s 
-                    operations. Currently, UBC Rapid has 7 working printers and 4 more 
-                    printers preparing for service. Most of these printers are set aside 
-                    for the 3D Printing service, which gives new members the opportunity 
-                    to work with clients and develop experience with 3D printing. However, 
-                    as these printers are used, they often require more 
-                    love in the form of replacement parts and monthly repairs.
-                </p>
-                <center><img src="/images/projects4.JPG" alt=""></img></center>
-                <br></br>
-                <h3>PRINTER AND MULTI-MATERIAL UPGRADES</h3>
-                <p>UBC Rapid aims to upgrade the current Prusa i3 MK2 printers with the 
-                    ability to print with multiple filaments at once. Using the official 
-                    Prusa i3 MK2 Multi Material upgrade kits, we will be able to 
-                    print up to four materials onto a single part.
-                </p>
-                <center><img src="/images/extruding.jpg" alt=""></img></center>
-                <br></br>
-                <h3>WORKSHOPS</h3>
-                <p>UBC Rapid hosts workshops to teach new recruits about 3D design and printing. 
-                    These workshops repeat every year after recruiting 
-                    new members. The workshops include skills required to work safely and efficiently in the 
-                    workspace. There are also specialized workshops in areas like web-development and PCB design.
-                    Workshops include, but are not limited to:
-                </p>
-                <ul>
-                    <li>How to use software required to operate UBC Rapid’s 3D printers</li>
-                    <li>Operating and troubleshooting 3D printers</li>
-                    <li>General 3D printer safety orientations</li>
-                    <li>PCB design, printing, and safety</li>
-                    <li>Website development and GitHub basics</li>
-                </ul>
-                <p>Workshops rotate between individual work and group work. By the end 
-                    of the workshops, recruits may operate printers for the UBC Rapid Printing Service. 
-                    As some workshop content is applicable to student life beyond 3D 
-                    printers, students who successfully complete the workshops will 
-                    have a future edge in leading teams or in jobs requiring fluency in 
-                    CAD software.
-                    <center><img src="/images/pcbworkshop.jpg" alt=""></img></center>
-                </p>
-                <br></br>
-                <h3>NUS Workshop</h3>
-                <p> info about the workshop
-                </p>
-                <center><img src="/images/NUS Workshop Exotic Materials.jpg" alt=""></img></center>
-                <center><img src="/images/NUS Worshop img 1.jpg" alt=""></img></center>
-            
+  return (
+    <>
+      <h1 className="projects">PROJECTS</h1>
 
-                
+      <div className="projects_wrapper">
+
+        {/* --- FILAMENT RECYCLER --- */}
+        <section className="project_section">
+          <h3>Filament Recycler – Design & Construction</h3>
+          <p>
+            To make 3D printing a more sustainable practice within UBC Rapid and the broader community,
+            our team is designing and building a filament recycler. This system transforms failed prints
+            and 3D-printing waste into fresh filament. It consists of a shredder, an extruder, and a spooler.
+            Recent progress includes completing the preliminary shredder and extruder designs and procuring
+            parts for the build phase.
+          </p>
+
+          <div className="image_grid">
+            <img src="/images/filament_recycler1.jpg" alt="Filament Recycler 1" />
+            <img src="/images/filament_recycler2.jpg" alt="Filament Recycler 2" />
+            <img src="/images/filament_recycler3.jpg" alt="Filament Recycler 3" />
+          </div>
+        </section>
+
+        {/* --- MAINTENANCE --- */}
+        <section className="project_section">
+          <h3>Maintenance</h3>
+          <p>
+            UBC Rapid currently operates 7 working 3D printers with 4 more being prepared for service.
+            These printers are essential for our 3D printing service, allowing members to gain hands-on
+            experience with clients. As they are used regularly, they require ongoing maintenance,
+            calibration, and part replacement.
+          </p>
+
+          <div className="image_center">
+            <img src="/images/projects4.JPG" alt="Printer maintenance" />
+          </div>
+        </section>
+
+        {/* --- MULTI-MATERIAL UPGRADES --- */}
+        <section className="project_section">
+          <h3>Printer & Multi-Material Upgrades</h3>
+          <p>
+            We aim to upgrade our Prusa i3 MK2 printers with the Multi-Material Upgrade, providing
+            the ability to print with up to four filaments simultaneously. This will enable the
+            team to produce more complex and functional prints for design teams and clients.
+          </p>
+
+          <div className="image_center">
+            <img className="img-medium" src="/images/extruding.jpg" alt="Multi-material printing" />
             </div>
-        </>
-    );
+        </section>
+
+        {/* --- WORKSHOPS --- */}
+        <section className="project_section">
+          <h3>Workshops</h3>
+          <p>
+            UBC Rapid hosts annual workshops to train new recruits in 3D design, 3D printer operation,
+            and technical skills needed to work safely and efficiently in the space.
+          </p>
+
+          <ul>
+            <li>Operating and troubleshooting 3D printers</li>
+            <li>General 3D-printer safety orientation</li>
+            <li>CAD design fundamentals</li>
+            <li>PCB design, production, and safety</li>
+            <li>Website development and GitHub basics</li>
+          </ul>
+
+          <div className="image_center">
+            <img src="/images/pcbworkshop.jpg" alt="PCB workshop" />
+          </div>
+        </section>
+
+        {/* --- INTERNATIONAL WORKSHOP SPOTLIGHT --- */}
+        <section className="project_section">
+          <h3>International Workshop Spotlight – NUS</h3>
+          <p>
+            UBC Rapid hosted a collaborative session with the National University of Singapore’s Centre
+            of Additive Manufacturing. We showcased our filament recycler and exotic materials testing project,
+            while learning about the advanced AM research happening at NUS. This marked our first international
+            academic connection, and we look forward to future collaboration.
+          </p>
+
+          <div className="image_grid">
+            <img src="/images/NUS Workshop Exotic Materials.jpg" alt="NUS workshop 1" />
+            <img src="/images/NUS Worshop img 1.jpg" alt="NUS workshop 2" />
+          </div>
+        </section>
+
+      </div>
+    </>
+  );
 }
