7f43dd9317aac069917f2d7e858a1065195bea7f
winterkube <theicekubex@gmail.com>
2025-11-30 23:38:04 -0800

    edited pages to have consistent paddings/margins for sides, also fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
     }
     const closeMobileMenu = () => setClick(false);
     const showButton = () => {
-        if(window.innerWidth <= 960) {
+        if(window.innerWidth <= 768) {
             setButton(false);
         } else {
             setButton(true);
