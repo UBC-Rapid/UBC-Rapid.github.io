8d9ce7ee8efaa327b8771c1ce807cd840515cdd8
Milun Gracias-Taplay <44041180+m1lun@users.noreply.github.com>
2026-09-06 20:01:42 -0700

    Re-enable 2026/27 application form and update dates

--- src/components/pages/JoinUs.js
+++ src/components/pages/JoinUs.js
@@ -9,7 +9,7 @@ export default function JoinUs() {
             <h1 className='join-us'>JOIN US</h1>
             <div className='joinus_wrapper'>
             <br /><br />
-                <h3>The applications for 2025/26 are now closed</h3>
+                <h3>Applications for 2026/27 are now open!</h3>
                 <br /><br />
                 <p>We are recruiting members to work in our sub-teams, as the demands for 
                     CAD Modelling, printer repair and 3D Printing orders have increased over the years. 
@@ -44,7 +44,7 @@ export default function JoinUs() {
                         buttonStyle='btn--primary'
                         buttonSize='btn--large'
                         newTab='true'
-                        link='https://forms.gle/rLQVo1o8oWQrb2Fw6'
+                        link='https://forms.gle/oxCytpJEqkYugr327'
                     >
                         Application Form
                     </Button>
